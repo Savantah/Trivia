04a664ecd23055f9277b8d0697e477ab8d36be78
Savantah <stherclair@gmail.com>
2024-10-31 00:02:34 -0400

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Star Lina on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
